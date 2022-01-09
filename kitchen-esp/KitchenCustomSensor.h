3116b73f21b6e2cdfe784bdb21ac183ec756204c
sergey-zalukaev <sergey.zalukaev@playrix.com>
2022-01-09 22:47:40 +0300

    Правки форматирования + wi-fi пароль для балкона

--- kitchen-esp/KitchenCustomSensor.h
+++ kitchen-esp/KitchenCustomSensor.h
@@ -5,24 +5,23 @@
 
 class KitchenCustomSensor : public PollingComponent, public Sensor {
 public:
-   
+
     KitchenCustomSensor(float val) 
         : PollingComponent(GLOBAL_DELAY) 
     {
         ds18b20TempBalconyStreet = new Sensor();
-       _aht10Inited = false;
-       _tempStreet = val;
+        _aht10Inited = false;
+        _tempStreet = val;
     }
 
-    void setup() override {
-       
+    void setup() override { 
     }
-  
+
     void update() override {
         ds18b20TempBalconyStreet->publish_state(_tempStreet);
         delay(GLOBAL_DELAY);
     }
- 
+
 public:
     Sensor* aht10Temp;
     Sensor* aht10Humidity;
@@ -52,5 +51,4 @@ private:
     const String PRESSURE_TAG = String("pressure");
     const String HUMIDITY_TAG = String("humidity"); 
     const String TEMP_STREET_TAG = String("temperature.street");
-  
 };
